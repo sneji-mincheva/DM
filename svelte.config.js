5b419c399a58621f11bba782ad8e205b9d27437d
SnejiMincheva <116427433+SnejiMincheva@users.noreply.github.com>
2025-07-23 23:10:46 +0300

    Deployment issues

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 export default {
 	kit: {
 		adapter: adapter()
